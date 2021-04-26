208b09686c8f69fabc4206fe6c901856a8278eb9
AnastasiyaZavalskaya <zavalskaya04@gmail.com>
2021-04-26 22:26:07 +0300

    components part2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string;
+  text: string;
+  id?: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Components';
+
+  posts: Post[] = [
+    {
+      title: 'Хочу выучить Ангулар Компненты',
+      text: 'Все еще хочу выучить',
+      id: 1
+    },
+    {
+      title: 'Заголовок 2',
+      text: 'Текст 2',
+      id: 2
+    }
+  ];
+
+  updatePosts(post: Post) {
+    this.posts.unshift(post);
+  }
 }
